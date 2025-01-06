c53da055c3ea4f294b8af8dca16cd1b79cbfd9e0
witeseb <tanatanee.wbst@mail.kmutt.ac.th>
2025-01-06 16:19:48 +0700

    Player controls and Map Revamp

---
+++ GAIA_unity/Assets/Scripts/Stage/StageManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using TMPro;
+
+public class StageManager : MonoBehaviour
+{
+    private Camera _cam;
+    private PlayerMovement _player;
+    [SerializeField] private Tilemap[] levels;
+    [SerializeField] private Transform spawnPoint;
+
+    [SerializeField] private TextMeshProUGUI nameText;
+
+    private int _currentPlayerTypeIndex;
+    private int _currentTilemapIndex;
+    private Color _currentForegroundColor;
+
+    public StageData stageData;
+
+    private void Awake()
+    {
+        _cam = FindFirstObjectByType<Camera>();
+        _player = GameObject.FindWithTag("Player").GetComponent<PlayerMovement>();
+    }
+
+    private void Start()
+    {
+        SetSceneData(stageData);
+    }
+
+    public void SetSceneData(StageData data)
+    {
+		stageData = data;
+
+        _cam.orthographicSize = data.camSize;
+        _cam.backgroundColor = data.backgroundColor;
+        //levels[_currentTilemapIndex].color = data.foregroundColor;
+
+        _currentForegroundColor = data.foregroundColor;
+    }
+}
