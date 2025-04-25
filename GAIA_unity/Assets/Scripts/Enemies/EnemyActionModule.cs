1f7926ac5a9e427de017650930ca2a30eb6fbb93
Intouch Yuso <act22117@gmail.com>
2025-04-25 14:43:25 +0700

    update environment #5

---
+++ GAIA_unity/Assets/Scripts/Enemies/EnemyActionModule.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class EnemyActionModule : MonoBehaviour
+{
+	public Enemy enemyCombat;
+
+	private void Awake()
+	{
+		if (enemyCombat == null)
+			enemyCombat = GetComponent<Enemy>();
+	}
+
+	public void TakeDamage(int amount, Vector2 hitSource)
+	{
+		AgentController agent = FindFirstObjectByType<AgentController>();
+		if (agent != null)
+		{
+			agent.AddReward(1.0f);
+		}
+		if (enemyCombat != null)
+			enemyCombat.TakeDamage(amount, hitSource);
+	}
+
+	public void Die()
+	{
+		AgentController agent = FindFirstObjectByType<AgentController>();
+		if (agent != null)
+		{
+			agent.AddReward(5.0f);
+		}
+
+		if (enemyCombat != null)
+			enemyCombat.Die();
+	}
+
+	// For future extension:
+	// public void MoveTo(Vector2 position)
+	// public void Attack()
+}
